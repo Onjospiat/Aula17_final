5793c6bf2dc4d0a258bac6bc45ec4264c1ed67e4
Onjospiat <guilherme.chini.gani@gmail.com>
2025-03-03 16:29:36 -0300

    Cödigo da página do projeto final da aula 17

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+function rolagem(id_secao){ //Declara a função com o parâmetro id_secao
+    const elemento = document.getElementById(id_secao); //define uma variável como um objeto de id igual ao parâmetro
+    if (elemento) { //Testa se o parâmetro usado existe.
+        elemento.scrollIntoView({ behavior: 'smooth' }); //Rola a tela até o objeto elemento
+        window.scrollBy({top:-80,behavior:'smooth'});
+     }
+     
+}
+
+window.rolagem = rolagem; //Define a função como global para poder ser acessada pelo HTML
