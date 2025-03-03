15f52b3a7027437e9218964f34a389a3d1cf63f3
Onjospiat <guilherme.chini.gani@gmail.com>
2025-03-03 16:32:43 -0300

    Correção do código JS: scroll agora funciona de forma correta

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function rolagem(id_secao){ //Declara a função com o parâmetro id_secao
     const elemento = document.getElementById(id_secao); //define uma variável como um objeto de id igual ao parâmetro
     if (elemento) { //Testa se o parâmetro usado existe.
         elemento.scrollIntoView({ behavior: 'smooth' }); //Rola a tela até o objeto elemento
-        window.scrollBy({top:-80,behavior:'smooth'});
      }
      
 }
